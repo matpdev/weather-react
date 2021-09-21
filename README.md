0ceb66cf34dec7767eaa1475b2a8e93bdf913dac
matpdev <matheus2ep@gmail.com>
2021-09-21 07:49:23 -0300

    v1

---
+++ README.md
@@ -0,0 +1,5 @@
+### Um app simples que indica a previsão do tempo no local que digitar
+
+npm run dev
+
+para liberar em localhost:3000
---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {api} from "./components/apiweather"
+import {FaTemperatureHigh, FaWind} from 'react-icons/fa'
+
+function App() {
+
+   const [weather, setWeather] = useState(null)
+   const [city, setCity] = useState('');
+
+   async function handleGetWeather() {
+      const response = await api.get(city);
+      setWeather(response.data);
+   }
+
+   useEffect(() => {
+      handleGetWeather()
+   }, [])
+
+   return (
+      <div className="App">
+
+         <header>
+            <form>
+               <input type="text"
+                  value={city}
+                  onChange={
+                     (e) => setCity(e.target.value)
+               }></input>
+               <button onSubmit={handleGetWeather}>enviar</button>
+            </form>
+         </header>
+         
+         {
+         weather && <main> {/*<p>{JSON.stringify(weather)}</p>*/}
+            <h1>{city}</h1>
+            <section>
+               <h2>Current Weather</h2>
+               <p> {
+                  weather.description
+               } </p>
+               <p> {
+                  weather.temperature
+               } </p>
+
+
+            </section>
+            <section>
+               <h2>Próximos Dias</h2>
+
+               {
+               weather.forecast.map(day => <li>
+                  <div>
+                     <p>
+                        <FaTemperatureHigh/> {
+                        day.temperature
+                     } </p>
+                     <p>
+                        <FaWind/> {
+                        day.wind
+                     } </p>
+                  </div>
+               </li>)
+            } </section>
+         </main>
+      } </div>
+   )
+}
+
+export default App
