c931f093b71638981f6de334d92c99d9740a68f2
Matpdev <matheus2ep@gmail.com>
2022-12-27 21:32:36 -0300

    Finally Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,57 @@
-import React, {useState, useEffect} from 'react'
-import logo from './logo.svg'
-import './App.css'
-import {api} from "./components/apiweather"
-import {FaTemperatureHigh, FaWind} from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { api } from "./components/apiweather";
+import { FaTemperatureHigh, FaWind } from "react-icons/fa";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
 
-   const [weather, setWeather] = useState(null)
-   const [city, setCity] = useState('');
+  async function handleGetWeather() {
+    const response = await api.get(city);
+    setWeather(response.data);
+  }
 
-   async function handleGetWeather() {
-      const response = await api.get(city);
-      setWeather(response.data);
-   }
-
-   useEffect(() => {
-      handleGetWeather()
-   }, [])
-
-   return (
-      <div className="App">
-
-         <header>
-            <form>
-               <input type="text"
-                  value={city}
-                  onChange={
-                     (e) => setCity(e.target.value)
-               }></input>
-               <button onSubmit={handleGetWeather}>enviar</button>
-            </form>
-         </header>
-         
-         {
-         weather && <main> {/*<p>{JSON.stringify(weather)}</p>*/}
-            <h1>{city}</h1>
-            <section>
-               <h2>Current Weather</h2>
-               <p> {
-                  weather.description
-               } </p>
-               <p> {
-                  weather.temperature
-               } </p>
-
-
-            </section>
-            <section>
-               <h2>Próximos Dias</h2>
-
-               {
-               weather.forecast.map(day => <li>
-                  <div>
-                     <p>
-                        <FaTemperatureHigh/> {
-                        day.temperature
-                     } </p>
-                     <p>
-                        <FaWind/> {
-                        day.wind
-                     } </p>
-                  </div>
-               </li>)
-            } </section>
-         </main>
-      } </div>
-   )
+  return (
+    <div className="App">
+      <header>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        ></input>
+        <button onClick={async () => await handleGetWeather()}>enviar</button>
+      </header>
+      {weather && (
+        <main>
+          {" "}
+          {/*<p>{JSON.stringify(weather)}</p>*/}
+          <h1>{city}</h1>
+          <section>
+            <h2>Current Weather</h2>
+            <p> {weather.description} </p>
+            <p> {weather.temperature} </p>
+          </section>
+          <section>
+            <h2>Próximos Dias</h2>
+            {weather.forecast.map((day) => (
+              <li>
+                <div>
+                  <p>
+                    <FaTemperatureHigh /> {day.temperature}{" "}
+                  </p>
+                  <p>
+                    <FaWind /> {day.wind}{" "}
+                  </p>
+                </div>
+              </li>
+            ))}{" "}
+          </section>
+        </main>
+      )}{" "}
+    </div>
+  );
 }
 
-export default App
+export default App;
